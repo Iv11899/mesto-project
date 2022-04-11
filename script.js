c680789d9706245553f0d9fb35b15b27658c7f3f
Alexander <deryugin.iv1@yandex.ru>
2022-04-11 23:33:25 +0300

    JS_01

---
+++ script.js
@@ -0,0 +1,118 @@
+const editProfile = document.querySelector('.profile__edit');
+const addProfile = document.querySelector('.profile__add');
+const editPopup = document.querySelector('.popup_edit')
+const imgPopup = document.querySelector('.popup_image')
+const popup = document.querySelector('.popup');
+const closeButtons = document.querySelectorAll('.popup__close');
+const submit = document.querySelector('popup__submit');
+const formElement = document.querySelector('.popup__form');
+const nameInput = formElement.querySelector('.popup__input_type_name');
+const aboutInput = formElement.querySelector('.popup__input_type_about');
+const PlacenameInput = document.querySelector('.popup__input_place-name');
+const PlacelinkInput = document.querySelector('.popup__input_place-link');
+const profileName = document.querySelector('.profile__name');
+const profileAbout = document.querySelector('.profile__about');
+const popupbigImg = document.querySelector('.popup_bigImg');
+const photoItem = document.querySelectorAll('.photo-grid__element');
+const popupbigImgCap = document.querySelector('.popup_bigImg__capture');
+const bigImg = document.querySelector('.popup_bigImg__image');
+const formElementImage = document.querySelector('.popup-image__form');
+const photoTemplate = document.querySelector('#photo-item').content;
+const photoElement =  photoTemplate.querySelector('.photo-grid__element').cloneNode(true);
+const photoGrid = document.querySelector('.photo-grid');
+
+// Открыть попап //
+function popupOpened(popup) {
+    popup.classList.add("popup_opened");
+  }
+  // Открыть попап c редактированием //
+  editProfile.addEventListener("click", function () {
+    popupOpened(editPopup);
+    initInfo();
+  });
+  
+  //Открыть попап добавления картинки //
+  addProfile.addEventListener("click", function () {
+    popupOpened(imgPopup);
+  });
+  
+  // Закрыть попап //
+  function popupClose(popup) {
+    popup.classList.remove("popup_opened");
+  }
+  
+  closeButtons.forEach(function (cl) {
+      cl.addEventListener("click", function () {
+      const popupOpened = document.querySelector(".popup_opened");
+      popupClose(popupOpened);
+    });
+  });
+  
+  // Установить начальные значения профиля редактирования //
+  function initInfo() {
+    nameInput.value = profileName.textContent;
+    aboutInput.value = profileAbout.textContent;
+  }
+  
+  // Обновить информацию в профиле //
+  function editProfileInfo(evt) {
+    evt.preventDefault();
+    profileName.textContent = nameInput.value;
+    profileAbout.textContent = aboutInput.value;
+    popupClose(editPopup);
+  }
+  
+  formElement.addEventListener("submit", editProfileInfo);
+  
+  // Функция открытия попап картинки //
+  
+  function openbigImg(element) {
+    const photoimg = element.querySelector(".photo-grid__img");
+    photoimg.addEventListener("click", function () {
+        popupOpened(popupbigImg);
+      const photoTitle = element.querySelector(".photo-grid__title").textContent;
+      bigImg.src = photoimg.src;
+      bigImg.alt = photoimg.alt;
+      popupbigImgCap.textContent = photoTitle;
+    });
+  }
+  
+  // Добавить карточку //
+  function initCard(image, title) {
+    const photoElement = photoTemplate
+      .querySelector(".photo-grid__element")
+      .cloneNode(true);
+      photoElement.querySelector(".photo-grid__img").src = image;
+      photoElement.querySelector(".photo-grid__title").textContent = title;
+    const heart = photoElement.querySelector(".photo-grid__like");
+    // добавление лайков
+    heart.addEventListener("click", function () {
+      heart.classList.toggle("photo-grid__like_active");
+    });
+    // Удалить карточку //
+    const deleteButton = photoElement.querySelector(".photo-grid__delete");
+    deleteButton.addEventListener("click", function () {
+        photoElement.remove();
+    });
+    // Открыть попап с изображением //
+    openbigImg(photoElement);
+    // Добавить карточку //
+    photoGrid.append(photoElement);
+  }
+  
+  // Создать карточку //
+  function createCard(evt) {
+    evt.preventDefault();
+    const name = PlacenameInput.value;
+    const link = PlacelinkInput.value;
+    initCard(link, name);
+    PlacenameInput.value = "";
+    PlacelinkInput.value = "";
+    popupClose(imgPopup);
+  }
+  
+  formElementImage.addEventListener("submit", createCard);
+  
+  initialCards.forEach((element) => {
+    initCard(element.link, element.name);
+  }); 
