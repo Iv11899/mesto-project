07edee145e62f95ea374b7bb1a206d4dae26b06f
Alexander <deryugin.iv1@yandex.ru>
2022-04-16 13:56:23 +0300

    JS_02

--- script.js
+++ script.js
@@ -18,101 +18,103 @@ const popupbigImgCap = document.querySelector('.popup_bigImg__capture');
 const bigImg = document.querySelector('.popup_bigImg__image');
 const formElementImage = document.querySelector('.popup-image__form');
 const photoTemplate = document.querySelector('#photo-item').content;
-const photoElement =  photoTemplate.querySelector('.photo-grid__element').cloneNode(true);
+const photoElement = photoTemplate.querySelector('.photo-grid__element').cloneNode(true);
 const photoGrid = document.querySelector('.photo-grid');
 
 // Открыть попап //
 function popupOpened(popup) {
-    popup.classList.add("popup_opened");
-  }
-  // Открыть попап c редактированием //
-  editProfile.addEventListener("click", function () {
-    popupOpened(editPopup);
-    initInfo();
+  popup.classList.add("popup_opened");
+}
+// Открыть попап c редактированием //
+editProfile.addEventListener("click", function () {
+  initInfo();
+  popupOpened(editPopup);
+});
+
+//Открыть попап добавления картинки //
+addProfile.addEventListener("click", function () {
+  popupOpened(imgPopup);
+});
+
+// Закрыть попап //
+function popupClose(popup) {
+  popup.classList.remove("popup_opened");
+}
+
+closeButtons.forEach(function (cl) {
+  cl.addEventListener("click", function () {
+    const popupOpened = document.querySelector(".popup_opened");
+    popupClose(popupOpened);
   });
-  
-  //Открыть попап добавления картинки //
-  addProfile.addEventListener("click", function () {
-    popupOpened(imgPopup);
+});
+
+// Установить начальные значения профиля редактирования //
+function initInfo() {
+  nameInput.value = profileName.textContent;
+  aboutInput.value = profileAbout.textContent;
+}
+
+// Обновить информацию в профиле //
+function editProfileInfo(evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileAbout.textContent = aboutInput.value;
+  popupClose(editPopup);
+}
+
+formElement.addEventListener("submit", editProfileInfo);
+
+function openBigImg(src, title) {
+  popupOpened(popupbigImg);
+  bigImg.src = src;
+  bigImg.alt = title;
+  popupbigImgCap.textContent = title;
+}
+
+// Добавить карточку //
+function initCard(image, title) {
+  const photoElement = photoTemplate
+    .querySelector(".photo-grid__element")
+    .cloneNode(true);
+  photoElement.querySelector(".photo-grid__title").textContent = title;
+  const photoImg = photoElement.querySelector(".photo-grid__img");
+  photoImg.src = image;
+  photoImg.alt = title;
+  photoImg.addEventListener("click", function () {
+    openBigImg(image, title)
   });
-  
-  // Закрыть попап //
-  function popupClose(popup) {
-    popup.classList.remove("popup_opened");
-  }
-  
-  closeButtons.forEach(function (cl) {
-      cl.addEventListener("click", function () {
-      const popupOpened = document.querySelector(".popup_opened");
-      popupClose(popupOpened);
-    });
+  const heart = photoElement.querySelector(".photo-grid__like");
+  // добавление лайков
+  heart.addEventListener("click", function () {
+    heart.classList.toggle("photo-grid__like_active");
   });
-  
-  // Установить начальные значения профиля редактирования //
-  function initInfo() {
-    nameInput.value = profileName.textContent;
-    aboutInput.value = profileAbout.textContent;
-  }
-  
-  // Обновить информацию в профиле //
-  function editProfileInfo(evt) {
-    evt.preventDefault();
-    profileName.textContent = nameInput.value;
-    profileAbout.textContent = aboutInput.value;
-    popupClose(editPopup);
-  }
-  
-  formElement.addEventListener("submit", editProfileInfo);
-  
-  // Функция открытия попап картинки //
-  
-  function openbigImg(element) {
-    const photoimg = element.querySelector(".photo-grid__img");
-    photoimg.addEventListener("click", function () {
-        popupOpened(popupbigImg);
-      const photoTitle = element.querySelector(".photo-grid__title").textContent;
-      bigImg.src = photoimg.src;
-      bigImg.alt = photoimg.alt;
-      popupbigImgCap.textContent = photoTitle;
-    });
-  }
-  
-  // Добавить карточку //
-  function initCard(image, title) {
-    const photoElement = photoTemplate
-      .querySelector(".photo-grid__element")
-      .cloneNode(true);
-      photoElement.querySelector(".photo-grid__img").src = image;
-      photoElement.querySelector(".photo-grid__title").textContent = title;
-    const heart = photoElement.querySelector(".photo-grid__like");
-    // добавление лайков
-    heart.addEventListener("click", function () {
-      heart.classList.toggle("photo-grid__like_active");
-    });
-    // Удалить карточку //
-    const deleteButton = photoElement.querySelector(".photo-grid__delete");
-    deleteButton.addEventListener("click", function () {
-        photoElement.remove();
-    });
-    // Открыть попап с изображением //
-    openbigImg(photoElement);
-    // Добавить карточку //
-    photoGrid.append(photoElement);
-  }
-  
-  // Создать карточку //
-  function createCard(evt) {
-    evt.preventDefault();
-    const name = PlacenameInput.value;
-    const link = PlacelinkInput.value;
-    initCard(link, name);
-    PlacenameInput.value = "";
-    PlacelinkInput.value = "";
-    popupClose(imgPopup);
-  }
-  
-  formElementImage.addEventListener("submit", createCard);
-  
-  initialCards.forEach((element) => {
-    initCard(element.link, element.name);
-  }); 
+  // Удалить карточку //
+  const deleteButton = photoElement.querySelector(".photo-grid__delete");
+  deleteButton.addEventListener("click", function () {
+    photoElement.remove();
+  });
+  return photoElement;
+}
+
+// Создать карточку //
+function createCard(evt) {
+  evt.preventDefault();
+  const name = PlacenameInput.value;
+  const link = PlacelinkInput.value;
+  popupClose(imgPopup);
+  renderCard(link, name);
+  PlacenameInput.value = "";
+  PlacelinkInput.value = "";
+}
+
+// Добавить карточку //
+function renderCard(image, title) {
+  const card = initCard(image, title);
+  photoGrid.prepend(card);
+}
+
+formElementImage.addEventListener("submit", createCard);
+
+initialCards.forEach((element) => {
+  renderCard(element.link, element.name);
+}); 
